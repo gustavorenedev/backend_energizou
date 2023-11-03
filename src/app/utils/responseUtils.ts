37978be3edcc305b8d0923ca901f49168619cae5
Gustavo René <renegustavo42@gmail.com>
2023-11-03 13:54:05 -0300

    feat: Add comments in response utils

--- src/app/utils/responseUtils.ts
+++ src/app/utils/responseUtils.ts
@@ -1,17 +1,21 @@
 import { Response } from "express";
 
+// Função para enviar uma resposta de sucesso (código 200) com os dados fornecidos
 export function sendSuccessResponse(res: Response, data: any) {
   return res.status(200).json(data);
 }
 
+// Função para enviar uma resposta de criação bem-sucedida (código 201) com os dados fornecidos
 export function sendCreatedResponse(res: Response, data: any) {
   return res.status(201).json(data);
 }
 
+// Função para enviar uma resposta sem conteúdo (código 204)
 export function sendNoContentResponse(res: Response) {
   return res.status(204).send();
 }
 
+// Função para enviar uma resposta de erro com o status e a mensagem fornecidos
 export function sendErrorResponse(
   res: Response,
   status: number,
